20aa57467b88311930f131f159468a18f747ecd7
NatalliaSavitskaya <138812097+NatalliaSavitskaya@users.noreply.github.com>
2024-08-27 14:34:51 +0300

    AppConfig.java is deleted as redundant; comment from ClientRepository.java is deleted

--- src/main/java/myApp/repository/ClientRepository.java
+++ src/main/java/myApp/repository/ClientRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends CrudRepository<Client, Integer> {
-    // Define any additional query methods if needed
+
 }
